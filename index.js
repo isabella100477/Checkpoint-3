b03ef5f41d8e3cd1d83a924d79054308d360eced
Jonathan Corley <jcorley@noscihigh.org>
2021-01-21 16:49:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Instructions:
+// Objects
+/*
+Declare a variable named me and 
+assign it to an object.
+
+And, assign me a key of firstName 
+and set it equal to your first name.
+
+Then, set another key of lastName 
+and set it equal to your last name
+*/
+
+
+
+
+
+// Arrays/Objects
+/*
+Create a hobbies array and fill the 
+array with string values of your favorite hobbies. 
+
+Add your hobbies array to your me object with 
+the key of myHobbies. (you must use dot 
+notation to add the above property). 
+
+Then, add a pet key to your me object, and assign 
+it a value of an object with 2 keys of name and 
+species that are set to the name of your pet and 
+its species. (if you don't have a pet, you can just 
+make one up)
+*/
